ff8657202f8d04b8f6f1e79ec9fd312da27ce14f
Richard Yi-Te Lu <yitelu@gmail.com>
2018-12-14 06:21:52 -0800

    Tab Fragment with PagerAdapter

---
+++ app/src/main/java/com/ytl/tabexperience/MainActivity.java
@@ -0,0 +1,53 @@
+package com.ytl.tabexperience;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label1));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label2));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label3));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        //Use PagerAdapter to manage page views in fragments.
+        //Each page is represented by its own fragment.
+        final ViewPager viewPager = findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        //Setting a listener for clicks.
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/ytl/tabexperience/PagerAdapter.java
@@ -0,0 +1,34 @@
+package com.ytl.tabexperience;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by Richard Yi-Te Lu on 14,December,2018
+ */
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    //Return the Fragment associated with a specified position.
+    @Override
+    public Fragment getItem(int i) {
+        switch (i){
+            case 0: return new TabFragment1();
+            case 1: return new TabFragment2();
+            case 2: return new TabFragment3();
+            default: return null;
+        }
+    }
+
+    //Return the number of views available.
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
